68a732ae523eace790b2722ae5aa5cb0dba6d6ca
hmelder <service@hugomelder.com>
2023-08-16 12:03:04 +0200

    Fix incorrect entrypoint invocation

--- Demo/InterPipelineCommunication/gst_intervideo_test.c
+++ Demo/InterPipelineCommunication/gst_intervideo_test.c
@@ -74,6 +74,6 @@ int main(int argc, char *argv[])
 #if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
     return gst_macos_main(intervideo_test, argc, argv, NULL);
 #else
-    return tutorial_main(argc, argv);
+    return intervideo_test(argc, argv);
 #endif
 }
