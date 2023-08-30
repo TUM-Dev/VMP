4f07858e14c82311724b7dca0006f46d78e6e2a8
hmelder <service@hugomelder.com>
2023-08-30 12:14:20 +0200

    Add v4l2 check, property get func, and change log levels

--- vmp-server/src/vmp-pipeline-manager.c
+++ vmp-server/src/vmp-pipeline-manager.c
@@ -13,6 +13,12 @@
 #include "vmp-common.h"
 #include "vmp-pipeline-manager.h"
 
+// For V4L2 device detection
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
+#include <unistd.h>
+
 /*
  * MARK: Functions
  */
@@ -27,6 +33,29 @@ static inline gboolean prevalidate_dev_path(gchar *path, GError **err)
     }
 
     return TRUE;
+}
+
+static void check_v4l2_device(gchar *device, GError **error)
+{
+    errno = 0;
+    int fd = open(device, O_RDWR);
+    if (fd == -1)
+    {
+        g_set_error(error, vmp_error_quark(), VMP_ERROR_DEVICE_NOT_AVAILABLE, "Could not open device %s: %s", device, g_strerror(errno));
+        return;
+    }
+
+    errno = 0;
+    struct v4l2_capability cap;
+    if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1)
+    {
+        g_set_error(error, vmp_error_quark(), VMP_ERROR_V4L2_ERRNO, "Could not query device %s: %s", device, g_strerror(errno));
+        return;
+    }
+
+    // TODO: Check V4L2 Flags
+
+    close(fd);
 }
 
 enum _VMPPipelineManagerProperty
@@ -47,6 +76,7 @@ typedef struct _VMPPipelineManagerPrivate
     gchar *channel;
     GstElement *pipeline;
     GstBus *bus;
+    GUdevClient *udev_client;
     gboolean receivedEOS;
     gboolean deviceConnected;
 } VMPPipelineManagerPrivate;
@@ -55,18 +85,32 @@ G_DEFINE_TYPE_WITH_PRIVATE(VMPPipelineManager, vmp_pipeline_manager, G_TYPE_OBJE
 
 // Forward Declarations
 static void vmp_pipeline_manager_set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec);
+static void vmp_pipeline_manager_get_property(GObject *object, guint property_id, GValue *value, GParamSpec *pspec);
 static void vmp_pipeline_manager_finalize(GObject *object);
+static void vmp_pipeline_manager_constructed(GObject *object);
 static void vmp_pipeline_manager_build_pipeline(VMPPipelineManager *mgr);
 static void vmp_pipeline_manager_restart_pipeline(VMPPipelineManager *mgr);
+
+static gboolean vmp_pipeline_manager_bus_cb(GstBus *bus, GstMessage *message, VMPPipelineManager *mgr);
+static void vmp_pipeline_manager_uevent_cb(GUdevClient *client, const gchar *action,
+                                           GUdevDevice *device, VMPPipelineManager *mgr);
 
 static void vmp_pipeline_manager_init(VMPPipelineManager *self)
 {
     VMPPipelineManagerPrivate *priv = vmp_pipeline_manager_get_instance_private(self);
 
+    /* NULL-terminated array of the udev subsystems we want to monitor
+     * for device changes.
+     * TODO: Generalise this
+     */
+    const gchar *const subsystems[] = {"video4linux", NULL};
+
     priv->src_device_name = NULL;
     priv->channel = NULL;
     priv->pipeline = NULL;
     priv->bus = NULL;
+    // Transfer: Full
+    priv->udev_client = g_udev_client_new(subsystems);
     priv->receivedEOS = FALSE;
     priv->deviceConnected = FALSE;
 }
@@ -76,7 +120,9 @@ static void vmp_pipeline_manager_class_init(VMPPipelineManagerClass *klass)
     GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
 
     gobject_class->set_property = vmp_pipeline_manager_set_property;
+    gobject_class->get_property = vmp_pipeline_manager_get_property;
     gobject_class->finalize = vmp_pipeline_manager_finalize;
+    gobject_class->constructed = vmp_pipeline_manager_constructed;
 
     obj_properties[PROP_SRC_DEVICE] = g_param_spec_string(
         "src-device",
@@ -119,6 +165,25 @@ static void vmp_pipeline_manager_set_property(GObject *object, guint property_id
     }
 }
 
+static void vmp_pipeline_manager_get_property(GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
+{
+    VMPPipelineManager *self = VMP_PIPELINE_MANAGER(object);
+    VMPPipelineManagerPrivate *priv = vmp_pipeline_manager_get_instance_private(self);
+
+    switch (property_id)
+    {
+    case PROP_SRC_DEVICE:
+        g_value_set_string(value, priv->src_device_name);
+        break;
+    case PROP_CHANNEL:
+        g_value_set_string(value, priv->channel);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+        break;
+    }
+}
+
 static void vmp_pipeline_manager_finalize(GObject *object)
 {
     VMPPipelineManager *self = VMP_PIPELINE_MANAGER(object);
@@ -127,7 +192,50 @@ static void vmp_pipeline_manager_finalize(GObject *object)
     g_free(priv->src_device_name);
     g_free(priv->channel);
 
+    g_object_unref(priv->udev_client);
+
+    if (priv->pipeline)
+    {
+        // Stop the pipeline if it's not in the NULL state
+        gst_element_set_state(priv->pipeline, GST_STATE_NULL);
+        // Unreference the pipeline
+        gst_object_unref(priv->pipeline);
+        priv->pipeline = NULL;
+    }
+
+    if (priv->bus)
+    {
+        // Unreference the bus
+        gst_object_unref(priv->bus);
+        priv->bus = NULL;
+    }
+
     G_OBJECT_CLASS(vmp_pipeline_manager_parent_class)->finalize(object);
+}
+
+static void vmp_pipeline_manager_constructed(GObject *object)
+{
+    VMPPipelineManager *self;
+    VMPPipelineManagerPrivate *priv;
+
+    self = VMP_PIPELINE_MANAGER(object);
+    priv = vmp_pipeline_manager_get_instance_private(self);
+
+    // Call the parent's constructed method
+    G_OBJECT_CLASS(vmp_pipeline_manager_parent_class)->constructed(object);
+
+    // Properties now available...
+    if (!prevalidate_dev_path(priv->src_device_name, NULL))
+    {
+        g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "Invalid device path: %s", priv->src_device_name);
+        return;
+    }
+
+    // Try to build pipeline
+    vmp_pipeline_manager_build_pipeline(self);
+
+    // Connect uevent callback
+    g_signal_connect(priv->udev_client, "uevent", G_CALLBACK(vmp_pipeline_manager_uevent_cb), self);
 }
 
 /*
@@ -168,7 +276,7 @@ static void vmp_pipeline_manager_uevent_cb(GUdevClient *client,
     const char *devpath = g_udev_device_get_device_file(device);
     if (!devpath)
     {
-        g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "gudev did not return a valid devpath in uevent callback");
+        g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "gudev did not return a valid devpath in uevent callback");
         return;
     }
 
@@ -258,7 +366,7 @@ static void vmp_pipeline_manager_build_pipeline(VMPPipelineManager *mgr)
     VMPPipelineManagerPrivate *mgr_priv;
     mgr_priv = vmp_pipeline_manager_get_instance_private(mgr);
 
-    // check_v4l2_device(mgr_priv->src_device_name, &err);
+    check_v4l2_device(mgr_priv->src_device_name, &err);
 
     if (err && (g_error_matches(err, vmp_error_quark(), VMP_ERROR_V4L2_ERRNO) || g_error_matches(err, vmp_error_quark(), VMP_ERROR_V4L2_NOT_SUPPORTED)))
     {
@@ -281,7 +389,7 @@ static void vmp_pipeline_manager_build_pipeline(VMPPipelineManager *mgr)
 
         if (err)
         {
-            g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_ERROR, "Failed to create pipeline: %s", err->message);
+            g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_WARNING, "Failed to create pipeline: %s", err->message);
 
             mgr_priv->pipeline = NULL;
             return;
@@ -300,7 +408,7 @@ static void vmp_pipeline_manager_build_pipeline(VMPPipelineManager *mgr)
     }
     else
     {
-        g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_ERROR,
+        g_log(VMP_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
               "Device %s could not be opened: %s. Monitoring v4l2 subsystem and waiting...",
               mgr_priv->src_device_name, err->message);
     }
