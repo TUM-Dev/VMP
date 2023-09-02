8c92a14fd014b832d8620310bbc1a1058ae86695
hmelder <service@hugomelder.com>
2023-09-02 23:08:20 +0200

    Implement V4L2 and ALSA managers

--- vmp-server/src/VMPPipelineManager.h
+++ vmp-server/src/VMPPipelineManager.h
@@ -5,6 +5,8 @@
  */
 
 #import <Foundation/Foundation.h>
+
+#import "VMPUdevClient.h"
 #import <gst/gst.h>
 
 extern NSString *const kVMPStateIdle;
@@ -14,54 +16,55 @@ extern NSString *const kVMPStateDeviceError;
 extern NSString *const kVMPStatePlaying;
 
 /**
-    @brief Delegate for pipeline state changes
+	@brief Delegate for pipeline state changes
 
-    When the pipeline manager is active, this delegate
-    is used to notify the client of state changes.
+	When the pipeline manager is active, this delegate
+	is used to notify the client of state changes.
 
-    Possible states are:
-    - kVMPStateIdle
-    - kVMPStateDeviceConnected
-    - kVMPStateDeviceDisconnected
-    - kVMPStateDeviceError
-    - kVMPStatePlaying
+	Possible states are:
+	- kVMPStateIdle
+	- kVMPStateDeviceConnected
+	- kVMPStateDeviceDisconnected
+	- kVMPStateDeviceError
+	- kVMPStatePlaying
 
-    @see VMPPipelineManager
+	@see VMPPipelineManager
 */
 @protocol VMPPipelineManagerDelegate <NSObject>
-@optional
-
 /**
-    @brief Called when the pipeline state changes
+	@brief Called when the pipeline state changes
 
-    @param state The new pipeline state
+	@param state The new pipeline state
 */
 - (void)onStateChanged:(NSString *)state;
 
+@optional
 /**
-    @brief Called when a GStreamer bus event is received
+	@brief Called when a GStreamer bus event is received
 
-    @param message The GStreamer bus message
+	@param message The GStreamer bus message
 
-    A bus event of the internal pipeline is sent to the delegate.
+	A bus event of the internal pipeline is sent to the delegate.
 */
 - (void)onBusEvent:(GstMessage *)message;
 @end
 
 /**
-    @brief Base class for pipeline managers
+	@brief Base class for pipeline managers
 
-    @property channel The GStreamer intervideo/interaudio channel name
-    @property delegate The delegate to notify of state changes
+	@property channel The GStreamer intervideo/interaudio channel name
+	@property delegate The delegate to notify of state changes
 
-    This class is used to manage a pipeline for a given channel.
-    It is meant to be subclassed for specific pipelines, and
-    not used directly.
+	This class is used to manage a pipeline for a given channel.
+	It is meant to be subclassed for specific pipelines, and
+	not used directly.
 */
 @interface VMPPipelineManager : NSObject
 
 @property (nonatomic, weak) id<VMPPipelineManagerDelegate> delegate;
 @property (nonatomic, readonly) NSString *channel;
+@property (nonatomic, readonly) NSString *state;
+@property (nonatomic, readonly) GstElement *pipeline;
 
 /**
    @brief Returns a pipeline manager initialized with the given channel
@@ -76,112 +79,117 @@ extern NSString *const kVMPStatePlaying;
 - (instancetype)initWithChannel:(NSString *)channel Delegate:(id<VMPPipelineManagerDelegate>)delegate;
 
 /**
-    @brief Starts the pipeline manager
+	@brief Starts the pipeline manager
 
-    @return YES if the pipeline manager was started successfully, NO otherwise
+	@return YES if the pipeline manager was started successfully, NO otherwise
 */
 - (BOOL)start;
+
+/**
+	@brief Stops the pipeline manager
+*/
+- (void)stop;
 @end
 
 /**
-    @brief Pipeline manager for V4L2 devices
+	@brief Pipeline manager for V4L2 devices
 
-    @property device The V4L2 device to use
+	@property device The V4L2 device to use
 
-    This class is used to manage a pipeline for a given V4L2 device.
-    It creates and manages a GStreamer pipeline, with the V4L2 device
-    as source, and a intervideosink with the given channel name as sink.
+	This class is used to manage a pipeline for a given V4L2 device.
+	It creates and manages a GStreamer pipeline, with the V4L2 device
+	as source, and a intervideosink with the given channel name as sink.
 
-    Once started, the pipeline manager actively monitors device events
-    for the given device. If the device is disconnected, the pipeline
-    is stopped and the state is set to kVMPStateDeviceDisconnected.
+	Once started, the pipeline manager actively monitors device events
+	for the given device. If the device is disconnected, the pipeline
+	is stopped and the state is set to kVMPStateDeviceDisconnected.
 
-    If the device is reconnected, the pipeline is created, or restarted
-    if it was already running, and the state is set to kVMPStateDeviceConnected.
-    If successful, the state is subsequently set to kVMPStatePlaying.
-    Otherwise, the state is set to kVMPStateDeviceError.o
+	If the device is reconnected, the pipeline is created, or restarted
+	if it was already running, and the state is set to kVMPStateDeviceConnected.
+	If successful, the state is subsequently set to kVMPStatePlaying.
+	Otherwise, the state is set to kVMPStateDeviceError.o
 
-    Initially, the state is set to kVMPStateIdle.
+	Initially, the state is set to kVMPStateIdle.
 
-    Other pipelines can hook into this pipeline by using the intervideosrc
-    with the same channel name. If the pipeline is stopped, the intervideosrc
-    will return a black screen.
+	Other pipelines can hook into this pipeline by using the intervideosrc
+	with the same channel name. If the pipeline is stopped, the intervideosrc
+	will return a black screen.
 
-    It is important to note that this inter pipeline mechanism only works
-    in the same process, and is not an inter-process communication mechanism.
+	It is important to note that this inter pipeline mechanism only works
+	in the same process, and is not an inter-process communication mechanism.
 */
-@interface VMPV4L2PipelineManager : VMPPipelineManager
+@interface VMPV4L2PipelineManager : VMPPipelineManager <VMPUdevClientDelegate>
 
 @property (nonatomic, readonly) NSString *device;
 
 /**
-    @brief Returns a pipeline manager initialized with the given device
+	@brief Returns a pipeline manager initialized with the given device
 
-    @param device The V4L2 device to use
-    @param channel The GStreamer intervideo/interaudio channel name
-    @param delegate The delegate to notify of state changes
+	@param device The V4L2 device to use
+	@param channel The GStreamer intervideo/interaudio channel name
+	@param delegate The delegate to notify of state changes
 
-    @return The initialized pipeline manager for the given device
-    */
+	@return The initialized pipeline manager for the given device
+	*/
 + (instancetype)managerWithDevice:(NSString *)device
-			  channel:(NSString *)channel
-			 Delegate:(id<VMPPipelineManagerDelegate>)delegate;
+						  channel:(NSString *)channel
+						 Delegate:(id<VMPPipelineManagerDelegate>)delegate;
 
 /**
-    @brief Initializes and returns a pipeline manager object
+	@brief Initializes and returns a pipeline manager object
 
-    @param device The V4L2 device to use
-    @param channel The GStreamer intervideo/interaudio channel name
-    @param delegate The delegate to notify of state changes
+	@param device The V4L2 device to use
+	@param channel The GStreamer intervideo/interaudio channel name
+	@param delegate The delegate to notify of state changes
 
-    @return A pipeline manager object initialized for the given device
+	@return A pipeline manager object initialized for the given device
 */
 - (instancetype)initWithDevice:(NSString *)device
-		       channel:(NSString *)channel
-		      Delegate:(id<VMPPipelineManagerDelegate>)delegate;
+					   channel:(NSString *)channel
+					  Delegate:(id<VMPPipelineManagerDelegate>)delegate;
 @end
 
 /**
-    @brief Pipeline manager for ALSA devices
+	@brief Pipeline manager for ALSA devices
 
-    @property device The ALSA device to use
+	@property device The ALSA device to use
 
-    This class is used to manage a pipeline for a given ALSA device.
-    It creates and manages a GStreamer pipeline, with the ALSA device
-    as source, and a interaudiosink with the given channel name as sink.
+	This class is used to manage a pipeline for a given ALSA device.
+	It creates and manages a GStreamer pipeline, with the ALSA device
+	as source, and a interaudiosink with the given channel name as sink.
 
-    The same device observation, and state handling as for V4L2 devices applies.
+	The same device observation, and state handling as for V4L2 devices applies.
 
-    Other pipelines can hook into this pipeline by using the interaudiosrc
-    with the same channel name.
+	Other pipelines can hook into this pipeline by using the interaudiosrc
+	with the same channel name.
 */
-@interface VMPAlsaPipelineManager : VMPPipelineManager
+@interface VMPALSAPipelineManager : VMPPipelineManager <VMPUdevClientDelegate>
 
 @property (nonatomic, readonly) NSString *device;
 
 /**
-    @brief Returns a pipeline manager initialized with the given device
+	@brief Returns a pipeline manager initialized with the given device
 
-    @param device The ALSA device to use
-    @param channel The GStreamer intervideo/interaudio channel name
-    @param delegate The delegate to notify of state changes
+	@param device The ALSA device to use
+	@param channel The GStreamer intervideo/interaudio channel name
+	@param delegate The delegate to notify of state changes
 
-    @return The initialized pipeline manager for the given device
+	@return The initialized pipeline manager for the given device
 */
 + (instancetype)managerWithDevice:(NSString *)device
-			  channel:(NSString *)channel
-			 Delegate:(id<VMPPipelineManagerDelegate>)delegate;
+						  channel:(NSString *)channel
+						 Delegate:(id<VMPPipelineManagerDelegate>)delegate;
 
 /**
-    @brief Initializes and returns a pipeline manager object
+	@brief Initializes and returns a pipeline manager object
 
-    @param device The ALSA device to use
-    @param channel The GStreamer intervideo/interaudio channel name
-    @param delegate The delegate to notify of state changes
+	@param device The ALSA device to use
+	@param channel The GStreamer intervideo/interaudio channel name
+	@param delegate The delegate to notify of state changes
 
-    @return A pipeline manager object initialized for the given device
+	@return A pipeline manager object initialized for the given device
 */
 - (instancetype)initWithDevice:(NSString *)device
-		       channel:(NSString *)channel
-		      Delegate:(id<VMPPipelineManagerDelegate>)delegate;
+					   channel:(NSString *)channel
+					  Delegate:(id<VMPPipelineManagerDelegate>)delegate;
 @end
--- vmp-server/src/VMPPipelineManager.m
+++ vmp-server/src/VMPPipelineManager.m
@@ -5,6 +5,7 @@
  */
 
 #import "VMPPipelineManager.h"
+#import "VMPErrors.h"
 
 // For V4L2 device detection
 #include <fcntl.h>
@@ -12,92 +13,400 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+// For ALSA device detection
+#include <sound/asound.h>
+
 NSString *const kVMPStateIdle = @"idle";
 NSString *const kVMPStateDeviceConnected = @"device_connected";
 NSString *const kVMPStateDeviceDisconnected = @"device_disconnected";
 NSString *const kVMPStateDeviceError = @"device_error";
 NSString *const kVMPStatePlaying = @"playing";
 
-@implementation VMPPipelineManager
+static gboolean gstreamer_bus_cb(GstBus *bus, GstMessage *message, VMPPipelineManager *mgr) { return TRUE; }
+
+// Redefine properties for readwrite access
+@interface VMPPipelineManager ()
+
+@property (nonatomic, readwrite) NSString *state;
+@property (nonatomic, readwrite) NSString *channel;
+@property (nonatomic, readwrite) GstElement *pipeline;
+
+// Pipeline management
+- (BOOL)_createPipelineWithLaunchArg:(NSString *)arg Error:(NSError **)error;
+- (BOOL)_resumePipelineWithError:(NSError **)error;
+- (void)_resetPipeline;
+
+@end
+
+@implementation VMPPipelineManager {
+  @protected
+	BOOL _pipelineCreated;
+}
 - (instancetype)initWithChannel:(NSString *)channel Delegate:(id<VMPPipelineManagerDelegate>)delegate {
-    self = [super init];
-    if (self) {
-	_channel = channel;
-	_delegate = delegate;
-    }
-    return self;
+	self = [super init];
+	if (self) {
+		_channel = channel;
+		_delegate = delegate;
+		_state = kVMPStateIdle;
+		_pipeline = NULL;
+		_pipelineCreated = NO;
+	}
+	return self;
 }
 
 - (BOOL)start {
-    NSAssert(NO, @"This method should be overriden and not called directly!");
-    return NO;
+	NSAssert(NO, @"This method should be overriden and not called directly!");
+	return NO;
 }
+
+- (void)stop {
+	NSAssert(NO, @"This method should be overriden and not called directly!");
+}
+
+/* Create a pipeline and return the status.
+   This method should only be called once during the lifetime of the object.
+   Subsequent calls will return NO.
+*/
+- (BOOL)_createPipelineWithLaunchArg:(NSString *)arg Error:(NSError **)error {
+	GstElement *pipeline;
+	GstBus *bus;
+	GstStateChangeReturn ret;
+	GError *gerror = NULL;
+
+	if (_pipelineCreated) {
+		return NO;
+	}
+
+	_pipelineCreated = YES;
+
+	// Transfer: Full
+	pipeline = gst_parse_launch([arg UTF8String], &gerror);
+	if (pipeline == NULL) {
+		NSLog(@"Failed to create pipeline");
+		if (gerror != NULL) {
+			NSLog(@"GStreamer error: %s", gerror->message);
+
+			if (error != NULL) {
+				NSDictionary *userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithUTF8String:gerror->message]};
+
+				*error = [NSError errorWithDomain:VMPErrorDomain
+											 code:VMPErrorCodeGStreamerParseError
+										 userInfo:userInfo];
+			}
+		}
+		return NO;
+	}
+
+	// Set pipeline state to playing
+	ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+	if (ret == GST_STATE_CHANGE_FAILURE) {
+		NSLog(@"Failed to start pipeline");
+		if (error != NULL) {
+			NSDictionary *userInfo = @{NSLocalizedDescriptionKey : @"Failed to start pipeline"};
+
+			*error = [NSError errorWithDomain:VMPErrorDomain
+										 code:VMPErrorCodeGStreamerStateChangeError
+									 userInfo:userInfo];
+		}
+		return NO;
+	}
+
+	// Transfer: Full
+	bus = gst_element_get_bus(pipeline);
+	if (bus != NULL) {
+		// Bridge object pointer without touching reference count
+		gst_bus_add_watch(bus, (GstBusFunc) gstreamer_bus_cb, (__bridge gpointer) self);
+		gst_object_unref(bus);
+	}
+
+	[self setPipeline:pipeline];
+	return YES;
+}
+
+- (BOOL)_resumePipelineWithError:(NSError **)error {
+	GstStateChangeReturn ret;
+
+	if ([self pipeline] == NULL) {
+		return NO;
+	}
+
+	ret = gst_element_set_state([self pipeline], GST_STATE_PLAYING);
+	if (ret == GST_STATE_CHANGE_FAILURE) {
+		NSLog(@"Failed to resume pipeline");
+		if (error != NULL) {
+			NSDictionary *userInfo = @{NSLocalizedDescriptionKey : @"Failed to resume pipeline"};
+
+			*error = [NSError errorWithDomain:VMPErrorDomain
+										 code:VMPErrorCodeGStreamerStateChangeError
+									 userInfo:userInfo];
+		}
+		return NO;
+	}
+
+	return YES;
+}
+
+- (void)_resetPipeline {
+	if ([self pipeline] != NULL) {
+		gst_element_set_state([self pipeline], GST_STATE_NULL);
+	}
+}
+
+- (void)dealloc {
+	gst_object_unref([self pipeline]);
+}
+
 @end
 
-@implementation VMPV4L2PipelineManager
+@implementation VMPV4L2PipelineManager {
+  @private
+	NSString *_pipelineDescription;
+	VMPUdevClient *_udevClient;
+}
+
 + (instancetype)managerWithDevice:(NSString *)device
-			  channel:(NSString *)channel
-			 Delegate:(id<VMPPipelineManagerDelegate>)delegate {
-    return [[VMPV4L2PipelineManager alloc] initWithDevice:device channel:channel Delegate:delegate];
+						  channel:(NSString *)channel
+						 Delegate:(id<VMPPipelineManagerDelegate>)delegate {
+	return [[VMPV4L2PipelineManager alloc] initWithDevice:device channel:channel Delegate:delegate];
+}
+
+- (instancetype)initWithDevice:(NSString *)device
+					   channel:(NSString *)channel
+					  Delegate:(id<VMPPipelineManagerDelegate>)delegate {
+	self = [super initWithChannel:channel Delegate:delegate];
+	if (self) {
+		_device = device;
+		_udevClient = [VMPUdevClient clientWithSubsystems:@[ @"video4linux" ] Delegate:self];
+		_pipelineDescription =
+			[NSString stringWithFormat:@"v4l2src device=%@ ! queue ! videoconvert ! queue ! intervideosink channel=%@",
+									   _device, [self channel]];
+	}
+	return self;
+}
+
+#pragma mark - VMPUdevClientDelegate methods
+
+- (void)onDeviceAdded:(NSString *)device {
+	if ([device isEqualToString:_device]) {
+		[self setState:kVMPStateDeviceConnected];
+		[[self delegate] onStateChanged:kVMPStateDeviceConnected];
+
+		if (!_pipelineCreated) {
+			if (![self _createPipelineWithLaunchArg:_pipelineDescription Error:NULL]) {
+				[self setState:kVMPStateDeviceError];
+				[[self delegate] onStateChanged:kVMPStateDeviceError];
+			} else {
+				[self setState:kVMPStatePlaying];
+				[[self delegate] onStateChanged:kVMPStatePlaying];
+			}
+		} else { // Resume pipeline
+			if (![self _resumePipelineWithError:NULL]) {
+				[self setState:kVMPStateDeviceError];
+				[[self delegate] onStateChanged:kVMPStateDeviceError];
+			} else {
+				[self setState:kVMPStatePlaying];
+				[[self delegate] onStateChanged:kVMPStatePlaying];
+			}
+		}
+	}
+}
+
+- (void)onDeviceRemoved:(NSString *)device {
+	if ([device isEqualToString:_device]) {
+		[self setState:kVMPStateDeviceDisconnected];
+		[[self delegate] onStateChanged:kVMPStateDeviceDisconnected];
+
+		[self _resetPipeline];
+	}
 }
 
 // Check if the device exists, and is a V4L2 device with the required
 // capabilities
 - (BOOL)_checkV4L2DeviceWithError:(NSError **)error {
-    errno = 0;
+	errno = 0;
 
-    int fd = open([_device UTF8String], O_RDWR);
-    if (fd == -1) {
-	NSLog(@"Failed to open device %@: %s", _device, strerror(errno));
+	int fd = open([_device UTF8String], O_RDWR);
+	if (fd == -1) {
+		NSLog(@"Failed to open device %@: %s", _device, strerror(errno));
 
-	if (error != NULL)
-	    *error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:nil];
+		if (error != NULL) {
+			NSDictionary *userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithUTF8String:strerror(errno)]};
+			*error = [NSError errorWithDomain:VMPErrorDomain code:VMPErrorCodeDeviceNotFound userInfo:userInfo];
+		}
+		close(fd);
+		return NO;
+	}
+
+	struct v4l2_capability cap;
+	// V4L2 devices support VIDIOC_QUERYCAP to optain information about the
+	// device
+	if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1) {
+		NSLog(@"Failed to query device %@: %s", _device, strerror(errno));
+
+		if (error != NULL) {
+			NSDictionary *userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithUTF8String:strerror(errno)]};
+			*error = [NSError errorWithDomain:VMPErrorDomain code:VMPErrorV4L2DeviceCapabilities userInfo:userInfo];
+		}
+		close(fd);
+		return NO;
+	}
+
 	close(fd);
-	return NO;
-    }
-
-    struct v4l2_capability cap;
-    // V4L2 devices support VIDIOC_QUERYCAP to optain information about the
-    // device
-    if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1) {
-	NSLog(@"Failed to query device %@: %s", _device, strerror(errno));
-
-	if (error != NULL)
-	    *error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:nil];
-	close(fd);
-	return NO;
-    }
-
-    close(fd);
-    return YES;
+	return YES;
 }
 
-- (instancetype)initWithDevice:(NSString *)device
-		       channel:(NSString *)channel
-		      Delegate:(id<VMPPipelineManagerDelegate>)delegate {
-    self = [super initWithChannel:channel Delegate:delegate];
-    if (self) {
-	_device = device;
-    }
-    return self;
+- (BOOL)start {
+	NSError *error = nil;
+
+	// Start pipeline immediately if the device exists, and is a valid V4L2 device
+	if (![self _checkV4L2DeviceWithError:&error]) {
+		if (error != nil && [error code] == VMPErrorCodeDeviceNotFound) {
+			[self setState:kVMPStateDeviceDisconnected];
+			[[self delegate] onStateChanged:kVMPStateDeviceDisconnected];
+		} else {
+			[self setState:kVMPStateDeviceError];
+			[[self delegate] onStateChanged:kVMPStateDeviceError];
+		}
+	} else { // Start pipeline immediately
+		[self setState:kVMPStateDeviceConnected];
+		[[self delegate] onStateChanged:kVMPStateDeviceConnected];
+
+		if (![self _createPipelineWithLaunchArg:_pipelineDescription Error:&error]) {
+			if (error != nil && [error code] == VMPErrorCodeGStreamerParseError) {
+				[self setState:kVMPStateDeviceError];
+				[[self delegate] onStateChanged:kVMPStateDeviceError];
+			}
+		}
+	}
+
+	if (![_udevClient startMonitorWithError:&error]) {
+		NSLog(@"Failed to start udev client: %@", error);
+		[self setState:kVMPStateDeviceError];
+		return NO;
+	}
+
+	return YES;
+}
+
+- (void)stop {
+	[self _resetPipeline];
+	[_udevClient stopMonitor];
 }
 
 @end
 
-@implementation VMPAlsaPipelineManager
+@implementation VMPALSAPipelineManager {
+  @private
+	NSString *_pipelineDescription;
+	VMPUdevClient *_udevClient;
+}
 + (instancetype)managerWithDevice:(NSString *)device
-			  channel:(NSString *)channel
-			 Delegate:(id<VMPPipelineManagerDelegate>)delegate {
-    return [[VMPAlsaPipelineManager alloc] initWithDevice:device channel:channel Delegate:delegate];
+						  channel:(NSString *)channel
+						 Delegate:(id<VMPPipelineManagerDelegate>)delegate {
+	return [[VMPALSAPipelineManager alloc] initWithDevice:device channel:channel Delegate:delegate];
 }
 
 - (instancetype)initWithDevice:(NSString *)device
-		       channel:(NSString *)channel
-		      Delegate:(id<VMPPipelineManagerDelegate>)delegate {
-    self = [super initWithChannel:channel Delegate:delegate];
-    if (self) {
-	_device = device;
-    }
-    return self;
+					   channel:(NSString *)channel
+					  Delegate:(id<VMPPipelineManagerDelegate>)delegate {
+	self = [super initWithChannel:channel Delegate:delegate];
+	if (self) {
+		_device = device;
+		_udevClient = [VMPUdevClient clientWithSubsystems:@[ @"sound" ] Delegate:self];
+		// TODO: Enforce S16LE right after alsasrc possible?
+		_pipelineDescription =
+			[NSString stringWithFormat:@"alsasrc device=%@ ! queue ! audioconvert ! capsfilter "
+									   @"caps=audio/x-raw,format=S16LE,layout=interleaved,channels=2 ! audioresample ! "
+									   @"queue ! interaudiosink channel=%@",
+									   _device, [self channel]];
+	}
+	return self;
 }
+
+#pragma mark - VMPUdevClientDelegate methods
+- (void)onDeviceAdded:(NSString *)device {
+	if ([device isEqualToString:_device]) {
+		[self setState:kVMPStateDeviceConnected];
+		[[self delegate] onStateChanged:kVMPStateDeviceConnected];
+	}
+}
+
+- (void)onDeviceRemoved:(NSString *)device {
+	if ([device isEqualToString:_device]) {
+		[self setState:kVMPStateDeviceDisconnected];
+		[[self delegate] onStateChanged:kVMPStateDeviceDisconnected];
+	}
+}
+
+- (BOOL)_checkAlsaDeviceWithError:(NSError **)error {
+	errno = 0;
+
+	int fd = open([_device UTF8String], O_RDWR);
+	if (fd == -1) {
+		NSLog(@"Failed to open device %@: %s", _device, strerror(errno));
+
+		if (error != NULL) {
+			NSDictionary *userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithUTF8String:strerror(errno)]};
+			*error = [NSError errorWithDomain:VMPErrorDomain code:VMPErrorCodeDeviceNotFound userInfo:userInfo];
+		}
+		close(fd);
+		return NO;
+	}
+
+	// Check if device is a sound card
+	errno = 0;
+	int isSoundCard = ioctl(fd, SNDRV_CTL_IOCTL_CARD_INFO, NULL);
+	if (isSoundCard < 0) {
+		NSLog(@"Failed to query device %@: %s", _device, strerror(errno));
+
+		if (error != NULL) {
+			NSDictionary *userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithUTF8String:strerror(errno)]};
+			*error = [NSError errorWithDomain:VMPErrorDomain code:VMPErrorALSADeviceCapabilities userInfo:userInfo];
+		}
+		close(fd);
+		return NO;
+	}
+
+	close(fd);
+	return YES;
+}
+
+- (BOOL)start {
+	NSError *error = nil;
+
+	// Start pipeline immediately if the device exists, and is a valid ALSA device
+	if (![self _checkAlsaDeviceWithError:&error]) {
+		if (error != nil && [error code] == VMPErrorCodeDeviceNotFound) {
+			[self setState:kVMPStateDeviceDisconnected];
+			[[self delegate] onStateChanged:kVMPStateDeviceDisconnected];
+		} else {
+			[self setState:kVMPStateDeviceError];
+			[[self delegate] onStateChanged:kVMPStateDeviceError];
+		}
+	} else { // Start pipeline immediately
+		[self setState:kVMPStateDeviceConnected];
+		[[self delegate] onStateChanged:kVMPStateDeviceConnected];
+
+		if (![self _createPipelineWithLaunchArg:_pipelineDescription Error:&error]) {
+			if (error != nil && [error code] == VMPErrorCodeGStreamerParseError) {
+				[self setState:kVMPStateDeviceError];
+				[[self delegate] onStateChanged:kVMPStateDeviceError];
+			}
+		}
+	}
+
+	if (![_udevClient startMonitorWithError:&error]) {
+		NSLog(@"Failed to start udev client: %@", error);
+		[self setState:kVMPStateDeviceError];
+		return NO;
+	}
+
+	return YES;
+}
+
+- (void)stop {
+	[self _resetPipeline];
+	[_udevClient stopMonitor];
+}
+
 @end
