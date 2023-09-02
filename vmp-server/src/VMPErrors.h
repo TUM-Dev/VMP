8211430f322a69283446efade356b913f69f0f39
hmelder <service@hugomelder.com>
2023-09-02 23:08:42 +0200

    Add ALSA error codes

--- vmp-server/src/VMPErrors.h
+++ vmp-server/src/VMPErrors.h
@@ -12,12 +12,18 @@ extern NSString *const VMPErrorDomain;
 @brief Error codes for the VMPErrorDomain
 */
 typedef NS_ENUM(NSInteger, VMPErrorCode) {
-    /// Unknown error
-    VMPErrorCodeUnknown = 0,
-    /// Device not found
-    VMPErrorCodeDeviceNotFound = 1,
-    /// Udev initialization error. Used in VMPUDevClient
-    VMPErrorCodeUdevError = 2,
-    /// Udev monitor initialization error. Used in VMPUDevClient
-    VMPErrorCodeUdevMonitorError = 3,
+	/// Unknown error
+	VMPErrorCodeUnknown = 0,
+	/// Device not found
+	VMPErrorCodeDeviceNotFound = 1,
+	/// V4L2 device capabilities error. Used in VMPV4L2PipelineManager
+	VMPErrorV4L2DeviceCapabilities = 2,
+	/// ALSA device capabilities error. Used in VMPALSAPipelineManager
+	VMPErrorALSADeviceCapabilities = 3,
+	/// Udev initialization error. Used in VMPUDevClient
+	VMPErrorCodeUdevError = 4,
+	/// Udev monitor initialization error. Used in VMPUDevClient
+	VMPErrorCodeUdevMonitorError = 5,
+	VMPErrorCodeGStreamerParseError = 6,
+	VMPErrorCodeGStreamerStateChangeError = 7,
 };
