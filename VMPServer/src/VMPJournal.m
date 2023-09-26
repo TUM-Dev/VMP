001b789d5ca23c8ad6d8b0eb815a1e14b7beac49
hmelder <service@hugomelder.com>
2023-09-27 00:57:23 +0200

    Color code direct stderr messages

--- VMPServer/src/VMPJournal.m
+++ VMPServer/src/VMPJournal.m
@@ -11,41 +11,50 @@
 // Generated project configuration
 #include "../build/config.h"
 
+#define ANSI_COLOR_FAINT "\x1b[2m" // Faint
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RED "\x1b[1;31m"		// Bold Red
+#define ANSI_COLOR_MAGENTA "\x1b[1;35m" // Bold Magenta
+#define ANSI_COLOR_RESET "\x1b[0m"
+
 #define LOG_PREAMBLE                                                                                                   \
 	va_list args;                                                                                                      \
 	va_start(args, format);                                                                                            \
 	NSString *message = [[NSString alloc] initWithFormat:format arguments:args];                                       \
 	va_end(args);
 
-#define SEND_LOG(prefix, type, message)                                                                                \
-	fprintf(stderr, "[ %s ] %s\n", prefix, [message UTF8String]);                                                      \
+#define SEND_LOG(prefix, color, type, message)                                                                         \
+	fprintf(stderr, "[ %s%s\x1b[0m ] %s\n", color, prefix, [message UTF8String]);                                      \
 	[[VMPJournal defaultJournal] message:message withPriority:type];
 
 void VMPDebug(NSString *format, ...) {
 #ifdef DEBUG
 	LOG_PREAMBLE
-	SEND_LOG("DEBUG", kVMPJournalTypeDebug, message);
+	fprintf(stderr, "[ %s%s\x1b[0m ]  \x1b[2m%s\x1b[0m\n", ANSI_COLOR_CYAN, "DEBUG", [message UTF8String]);
+	[[VMPJournal defaultJournal] message:message withPriority:kVMPJournalTypeDebug];
 #endif
 }
 
 void VMPInfo(NSString *format, ...) {
 	LOG_PREAMBLE
-	SEND_LOG("INFO", kVMPJournalTypeInfo, message);
+	SEND_LOG("INFO", ANSI_COLOR_GREEN, kVMPJournalTypeInfo, message);
 }
 
 void VMPWarn(NSString *format, ...) {
 	LOG_PREAMBLE
-	SEND_LOG("WARN", kVMPJournalTypeWarning, message);
+	SEND_LOG("WARN", ANSI_COLOR_YELLOW, kVMPJournalTypeWarning, message);
 }
 
 void VMPError(NSString *format, ...) {
 	LOG_PREAMBLE
-	SEND_LOG("ERROR", kVMPJournalTypeError, message);
+	SEND_LOG("ERROR", ANSI_COLOR_RED, kVMPJournalTypeError, message);
 }
 
 void VMPCritical(NSString *format, ...) {
 	LOG_PREAMBLE
-	SEND_LOG("CRITICAL", kVMPJournalTypeCritical, message);
+	SEND_LOG("CRITICAL", ANSI_COLOR_MAGENTA, kVMPJournalTypeCritical, message);
 }
 
 @interface VMPJournal ()
