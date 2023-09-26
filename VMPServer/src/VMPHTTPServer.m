18d3fa9480ae4b56af60c69fe98472a687e716bc
hmelder <service@hugomelder.com>
2023-09-27 01:07:15 +0200

    VMPHTTPServer: Add stubs

---
+++ VMPServer/src/VMPHTTPServer.m
@@ -0,0 +1,10 @@
+/* vmp-server - A virtual multimedia processor
+ * Copyright (C) 2023 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#import "VMPHTTPServer.h"
+
+@implementation VMPHTTPServer
+@end
