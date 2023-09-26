e9b39288aa786660296432ac0d906c39898cdb0a
hmelder <service@hugomelder.com>
2023-09-27 01:24:32 +0200

    VMPHTTPServer: Add API structure

--- VMPServer/src/VMPHTTPServer.m
+++ VMPServer/src/VMPHTTPServer.m
@@ -6,5 +6,37 @@
 
 #import "VMPHTTPServer.h"
 
+@implementation VMPHTTPURLRequest
+@end
+
+@interface VMPHTTPServer ()
+@property (nonatomic, readwrite) NSString *address;
+@property (nonatomic, readwrite) NSUInteger port;
+@end
+
 @implementation VMPHTTPServer
++ (VMPHTTPServer *)serverWithAddress:(NSString *)address port:(NSUInteger)port {
+	return [[VMPHTTPServer alloc] initWithAddress:address port:port];
+}
+
+- (instancetype)initWithAddress:(NSString *)address port:(NSUInteger)port {
+	self = [super init];
+	if (self) {
+		_address = address;
+		_port = port;
+	}
+
+	return self;
+}
+
+- (void)addHandlerForPath:(NSString *)path method:(NSString *)method handler:(id<VMPHTTPServerHandler>)handler {
+}
+- (void)addHandlerForPath:(NSString *)path
+				   method:(NSString *)method
+			responseBlock:(NSHTTPURLResponse * (^)(VMPHTTPURLRequest *) )responseBlock {
+}
+
+- (BOOL)startWithError:(NSError **)error {
+	return NO;
+}
 @end
