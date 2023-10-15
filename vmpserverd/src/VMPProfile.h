a2a048f9a4c49e3c6546a8c6ebe3d3069d120069
hmelder <service@hugomelder.com>
2023-10-15 16:04:12 +0200

    Add VMPProfile classes for managing pipeline profiles

---
+++ vmpserverd/src/VMPProfile.h
@@ -0,0 +1,137 @@
+/* vmp-server - A virtual multimedia processor
+ * Copyright (C) 2023 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#import <Foundation/Foundation.h>
+
+@class VMPProfile;
+
+/**
+	@brief Class for managing available profiles
+
+	Profiles are plist files that specify GStreamer
+	pipeline configurations for mountpoints and
+	video channels. They can be optimised for
+	specific platforms, and are scored upon start-up
+	to find the best-matching profile.
+
+	@see VMPProfile
+*/
+@interface VMPProfileManager : NSObject
+
+@property (nonatomic, readonly) NSString *runtimePlatform;
+@property (nonatomic, readonly) VMPProfile *currentProfile;
+@property (nonatomic, strong) NSArray<VMPProfile *> *availableProfiles;
+
++ (instancetype)managerWithPath:(NSString *)path
+				runtimePlatform:(NSString *)platform
+						  error:(NSError **)error;
+
+- (instancetype)initWithPath:(NSString *)path
+			 runtimePlatform:(NSString *)platform
+					   error:(NSError **)error;
+
+@end
+
+/**
+	@brief Class holding information about a profile
+
+	Profiles define the GStreamer pipelines for
+	mountpoints and channels for an array of supported
+	platforms.
+*/
+@interface VMPProfile : NSObject
+
+/**
+	@brief The title of the profile
+*/
+@property (nonatomic, strong) NSString *title;
+
+/**
+	@brief The reverse-domain identifier of the profile
+*/
+@property (nonatomic, strong) NSString *identifier;
+
+/**
+	@brief The version of the profile
+*/
+@property (nonatomic, strong) NSString *version;
+
+/**
+	@brief A human-readable description of the profile
+*/
+@property (nonatomic, strong) NSString *description;
+
+/**
+	@brief An array of supported platforms
+
+	Every profile has a compatibility score which
+	is used to select the best profile for the
+	current platform.
+
+	Currently, the following platforms are supported:
+	- "all" - Most generic profile. Matches with all platforms.
+	- "deepstream-6" - Platform supporting Nvidia Deepstream
+
+	Nvidia Deepstream is a multimedia framework that supplies
+	GStreamer elements making use of Nvidia technologies,
+	including NVENC, NVDEC, and CUDA.
+*/
+@property (nonatomic, strong) NSArray<NSString *> *supportedPlatforms;
+
+/**
+	@brief Pipeline configuration for supported mountpoints
+
+	Currently, the following mountpoints are implemented and
+	MUST be supported by a profile:
+	- "single" - A single video rtp stream
+	- "combined" - A combined video rtp stream with two video inputs
+
+	The following variables are allowed in the pipelines:
+	- {VIDEOCHANNEL.%u} - The video channel name. Enumerated with unsigned
+	  integers, starting at 0
+	- {PADEV} - Pulse Audio (PA) Device for audio pipelines
+*/
+@property (nonatomic, strong) NSDictionary<NSString *, NSDictionary *> *mountpoints;
+
+/**
+	@brief Pipeline configuration for video channels
+
+	The following channel types are implemented and MUST be
+	supported by a profile:
+	- "v4l2" - Video 4 Linux 2 (V4L2) video device source
+	- "videoTest" - Reproducable video test source
+
+	Allowed variables:
+	- {V4L2DEV} - V4L2 device path (e.g. /dev/video0)
+	- {VIDEOCHANNEL.%u}
+*/
+@property (nonatomic, strong) NSDictionary<NSString *, NSString *> *channels;
+
+/**
+	@brief Load a profile from a plist located at plistPath.
+
+	@param plistPath The path to the profile plist
+
+	@return A initialised VMPProfile object
+*/
++ (instancetype)profileWithPlist:(NSString *)plistPath error:(NSError **)error;
+
+- (instancetype)initWithPlist:(NSString *)plistPath error:(NSError **)error;
+
+/**
+	@brief Compatiblity score of profile (higher is better)
+
+	@parm platform The platform to calculate the score for
+
+	Profiles have an array of supported platforms.
+	If no platform in the array matches with the runtime
+	platform, the compatiblity score is -1.
+
+	Otherwise it is a non-negative integer. The optimal
+	profile for the runtime platform has the highest score.
+*/
+- (NSInteger)compatiblityScoreForPlatform:(NSString *)platform;
+@end
---
+++ vmpserverd/src/VMPProfile.m
@@ -0,0 +1,209 @@
+/* vmp-server - A virtual multimedia processor
+ * Copyright (C) 2023 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#import "VMPProfile.h"
+#import "VMPErrors.h"
+#import "VMPJournal.h"
+#include <Foundation/NSArray.h>
+#include <Foundation/NSFileManager.h>
+
+#define WRITE_ERROR(err, msg)                                                                      \
+	if (err) {                                                                                     \
+		*err = [NSError errorWithDomain:VMPErrorDomain                                             \
+								   code:VMPErrorCodeProfileError                                   \
+							   userInfo:@{NSLocalizedDescriptionKey : msg}];                       \
+	}
+
+@implementation VMPProfile
+
++ (instancetype)profileWithPlist:(NSString *)plistPath error:(NSError **)error {
+	return [[VMPProfile alloc] initWithPlist:plistPath error:error];
+}
+
+- (instancetype)initWithPlist:(NSString *)plistPath error:(NSError **)error {
+	VMP_ASSERT(plistPath, @"plistPath cannot be nil")
+
+	self = [super init];
+	if (self) {
+		NSDictionary *plist = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+		if (!plist) {
+			if (error) {
+				*error = [NSError errorWithDomain:VMPErrorDomain
+											 code:VMPErrorCodeProfileError
+										 userInfo:nil];
+			}
+			return nil;
+		}
+
+		_title = plist[@"title"];
+		_version = plist[@"version"];
+		_description = plist[@"description"];
+		_identifier = plist[@"identifier"];
+		if (!_title || !_version || !_description || !_identifier) {
+			WRITE_ERROR(error, @"Not all metadata (title, identifier, version, author) was found "
+							   @"in the profile plist");
+			return nil;
+		}
+
+		_supportedPlatforms = plist[@"supportedPlatforms"];
+		if (!_supportedPlatforms) {
+			WRITE_ERROR(error, @"Missing key in profile plist: supportedPlatforms");
+			return nil;
+		}
+		_mountpoints = plist[@"mountpoints"];
+		if (!_mountpoints) {
+			WRITE_ERROR(error, @"Missing key in profile plist: mountpoints");
+			return nil;
+		}
+
+		_channels = plist[@"channels"];
+		if (!_channels) {
+			WRITE_ERROR(error, @"Missing key in profile plist: channels");
+		}
+	}
+
+	return self;
+}
+
+- (NSInteger)compatiblityScoreForPlatform:(NSString *)platform {
+	NSUInteger length;
+	BOOL supportsAll;
+
+	VMP_ASSERT(_supportedPlatforms,
+			   @"supportedPlatforms must not be nil when calculating compatibilityScore");
+
+	length = [_supportedPlatforms count];
+	supportsAll = NO;
+
+	for (NSString *cur in _supportedPlatforms) {
+		if ([cur isEqualToString:platform]) {
+			return NSIntegerMax - (NSInteger) length;
+		}
+
+		if ([cur isEqualToString:@"all"]) {
+			supportsAll = YES;
+		}
+	}
+
+	if (supportsAll) {
+		return 1;
+	}
+
+	return -1;
+}
+
+@end
+
+// Declare runtimePlatform and currentProfile rw for internal use
+@interface VMPProfileManager ()
+
+@property (nonatomic, readwrite) NSString *runtimePlatform;
+@property (nonatomic, readwrite) VMPProfile *currentProfile;
+
+@end
+
+@implementation VMPProfileManager
+
++ (instancetype)managerWithPath:(NSString *)path
+				runtimePlatform:(NSString *)platform
+						  error:(NSError **)error {
+	return [[VMPProfileManager alloc] initWithPath:path runtimePlatform:platform error:error];
+}
+
+- (instancetype)initWithPath:(NSString *)path
+			 runtimePlatform:(NSString *)platform
+					   error:(NSError **)error {
+	self = [super init];
+
+	if (self) {
+		_runtimePlatform = platform;
+
+		// Sets availableProfiles property
+		if (![self _searchForProfilesInPath:path error:error])
+			return nil;
+
+		// Sets currentProfiles property
+		if (![self _selectBestProfileWithError:error])
+			return nil;
+	}
+
+	return self;
+}
+
+- (BOOL)_searchForProfilesInPath:(NSString *)path error:(NSError **)error {
+	NSFileManager *mgr;
+	NSArray<NSString *> *contents;
+	NSMutableArray<VMPProfile *> *found;
+
+	mgr = [NSFileManager defaultManager];
+	contents = [mgr contentsOfDirectoryAtPath:path error:error];
+	if (!contents) {
+		return NO;
+	}
+	found = [NSMutableArray arrayWithCapacity:[contents count]];
+
+	VMPProfile *pf;
+	for (NSString *cur in contents) {
+		if ([mgr isReadableFileAtPath:cur]) {
+			pf = [VMPProfile profileWithPlist:cur error:error];
+			if (!pf) {
+				return NO;
+			}
+
+			VMPDebug(@"Found profile: %@", [pf title]);
+			[found addObject:pf];
+		}
+	}
+
+	VMPDebug(@"Loaded %lu profiles", [found count]);
+	_availableProfiles = [NSArray arrayWithArray:found];
+
+	return YES;
+}
+
+- (BOOL)_selectBestProfileWithError:(NSError **)error {
+	VMP_ASSERT(_availableProfiles, @"availableProfiles property must not be nil!");
+
+	VMPProfile *curMax;
+
+	if ([_availableProfiles count] == 0) {
+		WRITE_ERROR(error, @"No profiles found");
+		return NO;
+	}
+
+	for (VMPProfile *p in _availableProfiles) {
+		NSInteger curMaxScore;
+		NSInteger selScore;
+
+		selScore = [p compatiblityScoreForPlatform:_runtimePlatform];
+		// Profile incompatible with runtime platform
+		if (selScore == -1)
+			continue;
+
+		if (!curMax) {
+			curMax = p;
+			continue;
+		}
+
+		curMaxScore = [curMax compatiblityScoreForPlatform:_runtimePlatform];
+
+		if (curMaxScore < selScore) {
+			VMPDebug(
+				@"%@ has score of %ld but %@ has score of %ld for platform %@. Selecting 2nd...",
+				[curMax title], curMaxScore, [p title], selScore, _runtimePlatform);
+			curMax = p;
+		}
+	}
+
+	if (!curMax) {
+		WRITE_ERROR(error, @"No profile found for runtime platform");
+		return NO;
+	}
+
+	return YES;
+}
+
+@end
