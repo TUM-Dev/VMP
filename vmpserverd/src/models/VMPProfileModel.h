c9f1811b306e44670346cb754596e99792154490
hmelder <service@hugomelder.com>
2023-10-17 15:25:57 +0200

    VMPProfileModel: Add compatiblityScoreForPlatform:, pipelineForChannelType:variables:error:, and pipelineForMountpointType:variables:error

--- vmpserverd/src/models/VMPProfileModel.h
+++ vmpserverd/src/models/VMPProfileModel.h
@@ -8,16 +8,55 @@
 
 #import "VMPPropertyListProtocol.h"
 
+/// Generic platform
+extern NSString *const VMPProfilePlatformAll;
+/// Nvidia Deepstream 6
+extern NSString *const VMPProfilePlatformDeepstream6;
+
+/**
+	@brief Class holding information about a profile
+
+	Profiles define the GStreamer pipelines for
+	mountpoints and channels for an array of supported
+	platforms.
+*/
 @interface VMPProfileModel : NSObject <VMPPropertyListProtocol>
 
+/**
+	@brief The name of the profile
+*/
 @property (nonatomic, strong) NSString *name;
 
+/**
+	@brief The reverse-domain identifier of the profile
+*/
 @property (nonatomic, strong) NSString *identifier;
 
+/**
+	@brief The version of the profile
+*/
 @property (nonatomic, strong) NSString *version;
 
+/**
+	@brief A human-readable description of the profile
+*/
 @property (nonatomic, strong) NSString *description;
 
+/**
+	@brief An array of supported platforms
+
+	Every profile has a compatibility score which
+	is used to select the best profile for the
+	current platform.
+
+	Currently, the following platforms are supported:
+	- "all" - Most generic profile. Matches with all platforms.
+	- "deepstream-6" - Platform supporting Nvidia Deepstream
+
+	Nvidia Deepstream is a multimedia framework that supplies
+	GStreamer elements making use of Nvidia technologies,
+	including NVENC, NVDEC, and CUDA.
+*/
 @property (nonatomic, strong) NSArray<NSString *> *supportedPlatforms;
 
 @property (nonatomic, strong) NSDictionary<NSString *, NSString *> *mountpoints;
@@ -26,7 +65,66 @@
 
 @property (nonatomic, strong) NSDictionary<NSString *, NSString *> *channels;
 
+/**
+	@brief Load a profile from a propertyList representation.
+
+	@param propertyList The property list representation
+	@param error Error pointer
+
+	@return A initialised VMPProfileModel object
+*/
 - (id)initWithPropertyList:(id)propertyList error:(NSError **)error;
+
+/**
+	@brief Convert the profile to a propertyList representation
+*/
 - (id)propertyList;
+
+/**
+	@brief Compatiblity score of profile (higher is better)
+
+	@param platform The platform to calculate the score for
+
+	Profiles have an array of supported platforms.
+	If no platform in the array matches with the runtime
+	platform, the compatiblity score is -1.
+
+	Otherwise it is a non-negative integer. The optimal
+	profile for the runtime platform has the highest score.
+*/
+- (NSInteger)compatiblityScoreForPlatform:(NSString *)platform;
+
+/**
+	@brief Process a pipeline template for a channel
+
+	@param type The type of the channel
+	@param variables A dictionary of variables to replace in the template
+	@param error Error pointer
+
+	@discussion This method processes a pipeline template with a substitution
+	dictionary of variables. The variables are replaced in the template, and
+	the resulting pipeline is returned.
+
+	@return A GStreamer pipeline description
+*/
+- (NSString *)pipelineForChannelType:(NSString *)type
+						   variables:(NSDictionary *)variables
+							   error:(NSError **)error;
+
+/**
+	@brief Process a pipeline template for a mountpoint
+
+	@param type The type of the mountpoint
+	@param variables A dictionary of variables to replace in the template
+	@param error Error pointer
+
+	@discussion Similar to pipelineForChannelType:variables:error:, but for
+	mountpoints.
+
+	@return A GStreamer pipeline description
+*/
+- (NSString *)pipelineForMountpointType:(NSString *)type
+							  variables:(NSDictionary *)variables
+								  error:(NSError **)error;
 
 @end
--- vmpserverd/src/models/VMPProfileModel.m
+++ vmpserverd/src/models/VMPProfileModel.m
@@ -5,9 +5,14 @@
  */
 
 #import "VMPProfileModel.h"
+#import "NSString+substituteVariables.h"
+#include "VMPConfigChannelModel.h"
 #import "VMPErrors.h"
 #import "VMPJournal.h"
 #import "VMPModelCommon.h"
+
+NSString *const VMPProfilePlatformAll = @"all";
+NSString *const VMPProfilePlatformDeepstream6 = @"deepstream6";
 
 @implementation VMPProfileModel
 - (id)initWithPropertyList:(id)propertyList error:(NSError **)error {
@@ -50,4 +55,74 @@
 		@"channels" : _channels
 	};
 }
+
+- (NSInteger)compatiblityScoreForPlatform:(NSString *)platform {
+	NSUInteger length;
+	BOOL supportsAll;
+
+	VMP_ASSERT(_supportedPlatforms,
+			   @"supportedPlatforms must not be nil when calculating compatibilityScore");
+
+	length = [_supportedPlatforms count];
+	supportsAll = NO;
+
+	for (NSString *cur in _supportedPlatforms) {
+		if ([cur isEqualToString:platform]) {
+			return NSIntegerMax - (NSInteger) length;
+		}
+
+		if ([cur isEqualToString:@"all"]) {
+			supportsAll = YES;
+		}
+	}
+
+	if (supportsAll) {
+		return 1;
+	}
+
+	return -1;
+}
+
+- (NSString *)_pipelineForType:(NSString *)type
+			templateDictionary:(NSDictionary<NSString *, NSString *> *)templateDictionary
+					 variables:(NSDictionary<NSString *, NSString *> *)variables
+						 error:(NSError **)error {
+	NSString *pipelineTemplate;
+	pipelineTemplate = [templateDictionary objectForKey:type];
+	if (!pipelineTemplate) {
+		VMP_FAST_ERROR(error, VMPErrorCodeProfileError, @"No pipeline template for type '%@'",
+					   type);
+		return nil;
+	}
+
+	return [pipelineTemplate stringBySubstitutingVariables:variables error:error];
+}
+
+- (NSString *)pipelineForChannelType:(NSString *)type
+						   variables:(NSDictionary *)variables
+							   error:(NSError **)error {
+	// Get the pipeline template from audioProviders if the type is audioTest or pulse
+	if ([type isEqualToString:VMPConfigChannelTypeAudioTest] ||
+		[type isEqualToString:VMPConfigChannelTypePulseAudio]) {
+		return [self _pipelineForType:type
+				   templateDictionary:_audioProviders
+							variables:variables
+								error:error];
+	}
+
+	return [self _pipelineForType:type
+			   templateDictionary:_channels
+						variables:variables
+							error:error];
+}
+
+- (NSString *)pipelineForMountpointType:(NSString *)type
+							  variables:(NSDictionary *)variables
+								  error:(NSError **)error {
+	return [self _pipelineForType:type
+			   templateDictionary:_mountpoints
+						variables:variables
+							error:error];
+}
+
 @end
