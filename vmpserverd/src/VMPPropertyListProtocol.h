61a47cb5ea176c19c4cb9f9e043c909a8dc24c12
hmelder <service@hugomelder.com>
2023-10-23 10:43:29 +0200

    VMPPropertyListProtocol: Add documentation

--- vmpserverd/src/VMPPropertyListProtocol.h
+++ vmpserverd/src/VMPPropertyListProtocol.h
@@ -7,6 +7,16 @@
 #import <Foundation/NSError.h>
 #import <Foundation/NSObject.h>
 
+/**
+	@brief A protocol for objects that can be serialized to and from a property list
+
+	@discussion This protocol is used to serialize and deserialize objects to and from a property
+   list representation. This means that all objects in the property list representation are
+   instances of NSString, NSNumber, NSArray, NSDictionary, or NSNull.
+
+	One can also serialise the property list representation to JSON using NSJSONSerialization.
+
+*/
 @protocol VMPPropertyListProtocol <NSObject>
 
 - (id)initWithPropertyList:(id)propertyList error:(NSError **)error;
