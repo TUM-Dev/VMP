42828afa15d5559ec5363f3425703762dc2ce428
hmelder <service@hugomelder.com>
2024-03-06 09:53:57 +0100

    NixOS Overlay: Create pkgconfig entry when building libdispatch

--- nix/libdispatch.nix
+++ nix/libdispatch.nix
@@ -1,18 +1,36 @@
-{ clangStdenv, lib, fetchFromGitHub, cmake, fetchpatch }:
+{ clangStdenv, lib, fetchFromGitHub, cmake, fetchpatch, writeText }:
 
 clangStdenv.mkDerivation rec {
   pname = "libdispatch";
-  version = "5.9.1";
+  version = "5.9.2";
 
   src = fetchFromGitHub {
     owner = "apple";
     repo = "swift-corelibs-libdispatch";
     rev = "swift-${version}-RELEASE";
-    hash = "sha256-TrZLw/3cPoUlMAeSUL78o/tuzczXLKc8sZw7brNFrKA=";
+    hash = "sha256-pta3wJj2LJ/lsYAWQpw0wSGLDMO41mN8Zbl78LUCaQo=";
   };
 
   nativeBuildInputs = [ cmake ];
   cmakeFlags = [ "-DCMAKE_INSTALL_LIBDIR=lib" ];
+    
+  pkgconfig = writeText "libdispatch.pc" ''
+    prefix=@out@
+    exec_prefix=@out@
+    libdir=@out@/lib
+    includedir=@out@/include
+
+    Name: libdispatch
+    Description: The libdispatch Project (Grand Central Dispatch) for concurrency on multicore hardware
+    Version: ${version}
+    Libs: -L@out@/lib -ldispatch
+    Cflags: -I@out@/include
+  '';
+
+  postInstall = ''
+    mkdir -p $out/lib/pkgconfig
+    sed "s|@out@|$out|g" ${pkgconfig} > $out/lib/pkgconfig/libdispatch.pc
+  '';
 
   meta = with lib; {
     description = "The libdispatch Project, (a.k.a. Grand Central Dispatch), for concurrency on multicore hardware";
