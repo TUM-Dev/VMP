4aec530c40959187491d12bd07b6b92591739865
hmelder <service@hugomelder.com>
2024-03-07 13:22:45 +0100

    vmpserverd: Implement BlockExecution Category

---
+++ Daemons/vmpserverd/src/NSRunLoop+blockExecution.h
@@ -0,0 +1,26 @@
+/* vmp-server - A virtual multimedia processor
+ * Copyright (C) 2024 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSRunLoop (blockExecution)
+
+/**
+ * Schedules a block to be executed with an initial delay, increasing the delay after each execution
+ * until a maximum delay is reached or the block returns YES.
+ *
+ * @param block The block to execute, which returns a BOOL. Return NO to continue execution with an
+ * increased delay, or YES to stop further executions.
+ * @param initialDelay The initial delay before the block is first executed.
+ * @param delayIncrement The amount by which the delay is increased after each execution.
+ * @param maxDelay The maximum delay between executions.
+ */
+- (void)scheduleBlock:(BOOL (^)(void))block
+		 initialDelay:(NSTimeInterval)initialDelay
+	   delayIncrement:(NSTimeInterval)delayIncrement
+			 maxDelay:(NSTimeInterval)maxDelay;
+
+@end
---
+++ Daemons/vmpserverd/src/NSRunLoop+blockExecution.m
@@ -0,0 +1,43 @@
+/* vmp-server - A virtual multimedia processor
+ * Copyright (C) 2024 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#import "NSRunLoop+blockExecution.h"
+
+@implementation NSRunLoop (BlockExecution)
+
+- (void)scheduleBlock:(BOOL (^)(void))block
+		 initialDelay:(NSTimeInterval)initialDelay
+	   delayIncrement:(NSTimeInterval)delayIncrement
+			 maxDelay:(NSTimeInterval)maxDelay {
+	NSMutableDictionary *userInfo = [@{
+		@"block" : [block copy],
+		@"delayIncrement" : @(delayIncrement),
+		@"maxDelay" : @(maxDelay),
+		@"currentDelay" : @(initialDelay)
+	} mutableCopy];
+
+	[self performSelector:@selector(executeBlockWithUserInfo:)
+			   withObject:userInfo
+			   afterDelay:initialDelay];
+}
+
+- (void)executeBlockWithUserInfo:(NSMutableDictionary *)userInfo {
+	BOOL (^block)(void) = userInfo[@"block"];
+	NSTimeInterval delayIncrement = [userInfo[@"delayIncrement"] doubleValue];
+	NSTimeInterval maxDelay = [userInfo[@"maxDelay"] doubleValue];
+	NSTimeInterval currentDelay = [userInfo[@"currentDelay"] doubleValue];
+
+	BOOL shouldStop = block();
+	if (!shouldStop) {
+		currentDelay = MIN(currentDelay + delayIncrement, maxDelay);
+		userInfo[@"currentDelay"] = @(currentDelay);
+		[self performSelector:@selector(executeBlockWithUserInfo:)
+				   withObject:userInfo
+				   afterDelay:currentDelay];
+	}
+}
+
+@end
