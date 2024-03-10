91588990efbc9df49e79696c63efb8c0af8d7104
hmelder <service@hugomelder.com>
2024-03-10 17:29:30 +0100

    vmpserverd: Add RecordingManager stubs

---
+++ Daemons/vmpserverd/src/VMPRecordingManager.h
@@ -0,0 +1,48 @@
+/* vmpserverd - A virtual multimedia processor
+ * Copyright (C) 2024 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#import "VMPPipelineManager.h"
+
+/// The encoding bitrate
+extern NSString *const kVMPRecordingBitrate;
+
+/**
+ * @brief Recording Manager
+ *
+ * A subclass of th pipeline manager that
+ * has extensions for recording to a
+ * file.
+ */
+@interface VMPRecordingManager : VMPPipelineManager
+
+
+/**
+ * Absolute destination path of recording.
+ */
+@property (nonatomic, readonly) NSURL *path;
+
+/**
+ * Additional recording and encoding options.
+ */
+@property (nonatomic, readonly) NSDictionary *options;
+
++ (instancetype)recorderWithChannel: (NSString *)channel
+			 				   path: (NSURL *)path
+   					    recordUntil: (NSDate *) date
+						    options: (NSDictionary *)options
+					       delegate: (id<VMPPipelineManagerDelegate>)delegate;
+
+- (instancetype)initWithChannel: (NSString *)channel
+						   path: (NSURL *)path
+   					recordUntil: (NSDate *) date
+						options: (NSDictionary *)options
+					   delegate: (id<VMPPipelineManagerDelegate>)delegate NS_DESIGNATED_INITIALIZER;
+
+- (BOOL) changeDeadline: (NSDate *)date;
+
+- (NSDate *) deadline;
+
+@end
---
+++ Daemons/vmpserverd/src/VMPRecordingManager.m
@@ -0,0 +1,42 @@
+/* vmpserverd - A virtual multimedia processor
+ * Copyright (C) 2024 Hugo Melder
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+ #import "VMPRecordingManager.h"
+
+ NSString *const kVMPRecordingBitrate = @"recordingBitrate";
+
+@implementation VMPRecordingManager
+{
+	NSDate *_deadline;
+}
+
++ (instancetype)recorderWithChannel: (NSString *)channel
+			 				   path: (NSURL *)path
+   					    recordUntil: (NSDate *) date
+						    options: (NSDictionary *)options 
+						   delegate: (id<VMPPipelineManagerDelegate>)delegate {
+	return nil;
+}
+
+- (instancetype)initWithChannel: (NSString *)channel
+						   path: (NSURL *)path
+   					recordUntil: (NSDate *) date
+						options: (NSDictionary *)options
+ 				   	   delegate: (id<VMPPipelineManagerDelegate>)delegate {
+	self = [super initWithLaunchArgs: @"" channel: channel delegate: delegate];
+	return self;
+
+}
+
+- (BOOL) changeDeadline: (NSDate *)date {
+	return NO;
+}
+
+- (NSDate *) deadline {
+	return nil;
+}
+
+@end
